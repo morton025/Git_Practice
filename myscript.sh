4b8e498050e7a6ba89908dde3a662f49bbc6f711
pumacore <pumacore22@gmail.com>
2020-12-08 12:03:27 +0200

    Done script

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dir=$(git log --pretty=format:"Hash:%h; Name:%s; Time:%cr;")
 write=$(echo "$dir/n" > result.txt)
-info=$(grep 'Add script' result.txt)
+info=$(grep 'script' result.txt)
 echo "$info"
