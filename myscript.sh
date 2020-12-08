c1de9efcbc863995ec1c939370e30ae89d3abec6
pumacore <pumacore22@gmail.com>
2020-12-08 11:29:20 +0200

    Modify script

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir=$(git log --pretty=format:"%h---%s")
-write=$(echo $dir > result.txt)
-info=$(grep 'Initial' result.txt)
-echo $dir
+dir=$(git log --pretty=format:"Hash:%h; Name:%s; Time:%cr;")
+write=$(echo "$dir/n" > result.txt)
+info=$(grep 'Add script' result.txt)
+echo "$info"
